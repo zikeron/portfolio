dd203692bbcd0cb4d5b8b895f2f2f17ed7c2c523
Alejandro Cortez <acortez.dev@gmail.com>
2020-10-28 10:29:58 -0600

    Some tricks to render 100 on devices

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import { App } from './containers/App';
 
 let vh = window.innerHeight * 0.01;
-console.log('vh', vh);
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 ReactDOM.render(<App />, document.getElementById('app'));
