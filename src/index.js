78e1d01f0bcb781495dd53bb4bd685b13b3d1567
Alejandro Cortez <acortez.dev@gmail.com>
2020-10-28 10:19:35 -0600

    Some code to render 100% heigh in devices

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './containers/App';
 
+let vh = window.innerHeight * 0.01;
+console.log('vh', vh);
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 ReactDOM.render(<App />, document.getElementById('app'));
